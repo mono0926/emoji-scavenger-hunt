6968bf0dad7ebdb0d231bdc5bed7810707e58a4e
Masayuki Ono <mono0926@gmail.com>
2019-01-19 14:20:57 +0900

    Add project

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text(
+          'aaa',
+          style: TextStyle(fontFamily: 'Changa One'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:emoji_scavenger_hunt/app.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
