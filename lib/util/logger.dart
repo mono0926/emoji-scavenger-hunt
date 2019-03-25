2d35fcd3ddd2cb99cb63398cd874456d7e93d760
Masayuki Ono <mono0926@gmail.com>
2019-03-25 21:24:34 +0900

    Use logger print mode

--- lib/util/logger.dart
+++ lib/util/logger.dart
@@ -2,6 +2,7 @@ import 'package:simple_logger/simple_logger.dart';
 
 // TODO: stacktraceEnabledはリリースビルドではfalseにする
 final logger = SimpleLogger()
+  ..mode = LoggerMode.print
   ..setLevel(
     Level.FINEST,
     includeCallerInfo: true,
